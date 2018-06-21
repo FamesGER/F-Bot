9cd62072987295d75a89a79b4766298241eecd72
FamesGER <jonasdoedtmann@hotmail.de>
2018-06-21 14:29:40 +0200

    BotTest

---
+++ BotTest.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+
+import time
+
+Client = discord.Client()
+bot= commands.Bot(command_prefix = "!") #use this prefix for commands
+
+
+@bot.event
+async def on_ready():
+	print("Bot is ready")
+
+@bot.command(pass_context=True)
+async def ping(ctx): #ping is the actual command name
+	await bot.say(":ping_pong: ping!")
+	print("Ping command")
+@bot.command(pass_context=True)
+async def info(ctx, user: discord.Member):
+	if user.game == None: #check if the user is playing anything
+		user.game = "Nothing"
+	await bot.say("The username is: {}".format(user.name))
+	await bot.say("The user is playing: " + str(user.game))
+	await bot.say(user.name + " is " + str(user.status))
+@info.error #if !info errors
+async def test_on_error(ctx,error):
+	bot.say("No user input")
+
+
+
+
+
+bot.run("NDU5MDkwODMwMzMwNjkxNTk0.DgxKlg.UbQSu8ZJjCG2SQcCziZkq1jZvyI")
