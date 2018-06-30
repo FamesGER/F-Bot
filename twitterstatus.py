9784a68646ec0927e0de2a88ed6ceb7ac29f32f2
FamesGER <jonasdoedtmann@hotmail.de>
2018-06-30 19:22:41 +0200

    Add files via upload

---
+++ twitterstatus.py
@@ -0,0 +1,22 @@
+
+class getTweet(object):
+	import tweepy
+	from tweepy import OAuthHandler
+	consumer_key = os.environ.get('c_key')
+	consumer_secret = os.environ.get('c_secret')
+	access_token = os.environ.get('a_token')
+	access_secret = os.environ.get('a_secret')
+
+	auth = OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_secret)
+	 
+	twitterAPI = tweepy.API(auth)
+
+	tweet = twitterAPI.user_timeline(screen_name = '@Ducks_Daily', count = 1, include_rts = False, include_entities =True) #get 1 tweet from Ducks Daily 
+	def tweetMedia():
+		for media in getTweet.tweet:
+			return (media.entities['media'][0]['media_url_https'])
+	def tweetStatus():
+		for status in getTweet.tweet:
+			return (status.entities['media'][0]['expanded_url'])
+		
