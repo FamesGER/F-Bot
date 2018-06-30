65d6a7b6e4a8297b4c43b9edf1065805acc763ae
FamesGER <jonasdoedtmann@hotmail.de>
2018-06-30 19:57:10 +0200

    Update twitterstatus.py

--- twitterstatus.py
+++ twitterstatus.py
@@ -9,17 +9,16 @@ class getTweet():
 	import tweepy
 	from tweepy import OAuthHandler
 
-
-	auth = OAuthHandler(c_key, c_secret)
-	auth.set_access_token(a_token, a_secret)
+	auth = OAuthHandler(self.c_key, self.c_secret)
+	auth.set_access_token(self.a_token, self.a_secret)
 	 
 	twitterAPI = tweepy.API(auth)
 
 	tweet = twitterAPI.user_timeline(screen_name = '@Ducks_Daily', count = 1, include_rts = False, include_entities =True) #get 1 tweet from Ducks Daily 
+	
 	def tweetMedia():
 		for media in getTweet.tweet:
 			return (media.entities['media'][0]['media_url_https'])
 	def tweetStatus():
 		for status in getTweet.tweet:
 			return (status.entities['media'][0]['expanded_url'])
-		
