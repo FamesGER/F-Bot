3ab138522684e7ba6ff8789cc0e0878d5676f843
FamesGER <jonasdoedtmann@hotmail.de>
2018-07-01 00:30:18 +0200

    Add files via upload

---
+++ bothelp.py
@@ -0,0 +1,12 @@
+def commandHelp():
+	text = ("```"
+			"!killfbot - Bot owner only.\n"
+           	"!ping - Pong! \n"
+			"!info - Info about yourself or pinged user.\n"
+			"!say - Makes the bot say something. \n"
+			"!loss - Gain.\n"
+			"!sadcat - Real sadcat hours.\n"
+			"!duck - latest duck tweet from @DucksDaily"
+           "```")
+	return text
+	
