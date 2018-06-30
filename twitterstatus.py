66c5f20b635e072011ae80814dcd018fb1287270
FamesGER <jonasdoedtmann@hotmail.de>
2018-06-30 20:55:29 +0200

    Update twitterstatus.py

--- twitterstatus.py
+++ twitterstatus.py
@@ -17,9 +17,9 @@ class getTweet():
 		twitterAPI = tweepy.API(auth)
 		tweet = twitterAPI.user_timeline(screen_name = '@Ducks_Daily', count = 1, include_rts = False, include_entities =True) #get 1 tweet from Ducks Daily 
 	
-		def media():
-			for media in getTweet.tweet:
-				return (media.entities['media'][0]['media_url_https'])
-		def status():
+	def tweetMedia():
+		for media in getTweet.tweet:
+			return (media.entities['media'][0]['media_url_https'])
+	def tweetStatus():
 			for status in getTweet.tweet:
-				return (status.entities['media'][0]['expanded_url'])
+				eturn (status.entities['media'][0]['expanded_url'])
