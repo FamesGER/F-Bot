c0825c10df50f0e4f53529db8a492c7a744bf273
FamesGER <jonasdoedtmann@hotmail.de>
2018-07-02 00:40:29 +0200

    Update botgspread.py

--- botgspread.py
+++ botgspread.py
@@ -1,23 +1,20 @@
 class botgspread():
-	def __init__(self,sheetName= 'F-Botsheet',jsonFile= ''):
+	def __init__(self,sheetName= 'F-Botsheet'):
 		self.sName = sheetName	
-		self.jFile = jsonFile
 
 	def getClient(self): #create the client
 		import gspread
 		from oauth2client.service_account import ServiceAccountCredentials
 		scope = ['https://spreadsheets.google.com/feeds',
 		         'https://www.googleapis.com/auth/drive']
-		#creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json',scope)
-		creds = ServiceAccountCredentials.from_json_keyfile_dict(self.jFile,scope)
-
+		creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json',scope)
 		client = gspread.authorize(creds)
 		return client
+	
 	def getSheet(self): #access the sheet
 		import gspread
 		from oauth2client.service_account import ServiceAccountCredentials#
 		import pprint
-
 		sheet = self.getClient().open(self.sName).sheet1
 		return sheet
 
