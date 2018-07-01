5069a653ec5e6295d9b8de1a23b03137826e009d
FamesGER <jonasdoedtmann@hotmail.de>
2018-07-01 14:45:29 +0200

    Update twitterstatus.py

--- twitterstatus.py
+++ twitterstatus.py
@@ -1,7 +1,4 @@
 class getTweet():
-	import tweepy
-	from tweepy import OAuthHandler
-	
 	def __init__(self, consumer_key, consumer_secret, access_token, access_secret, twitterUser = "@Ducks_Daily", amount = 1):
 		self.c_key = consumer_key
 		self.c_secret = consumer_secret
@@ -10,14 +7,17 @@ class getTweet():
 		self.tUser = twitterUser
 		self.tAmount= amount	
 		
-	def tweet(self):
+	def tweetAPI(self): #get twitter API
 		import tweepy
 		from tweepy import OAuthHandler
 		auth = OAuthHandler(self.c_key, self.c_secret)
 		auth.set_access_token(self.a_token, self.a_secret)
 
-		twitterAPI = tweepy.API(auth)
-		tweet = twitterAPI.user_timeline(screen_name = self.tUser, count = self.tAmount	, include_rts = False, include_entities =True) #get 1 tweet from Ducks Daily 
+		twitterAPI = tweepy.API(auth,wait_on_rate_limit=True)
+		return twitterAPI
+
+	def tweet(self, customAmount = 1): #get a tweet
+		tweet = self.tweetAPI().user_timeline(screen_name = self.tUser, count = customAmount	, include_rts = False, include_entities =True) #get n tweet from Ducks Daily 
 		return tweet
 	
 	def tweetStatus(self):
@@ -27,3 +27,17 @@ class getTweet():
 				except: #if it wasnt a tweet with media
 					returnText = status.text
 					return returnText
+
+	def tweetRandom(self):
+		import random
+		randomAmount = 100
+		tweet = self.tweet(customAmount= randomAmount)
+		statusList = [];
+		for status in tweet:
+			try:
+				statusList.append(status.entities['media'][0]['expanded_url']);
+			except:
+				continue
+			#return (status.entities['media'][0]['expanded_url'])
+
+		return random.choice(statusList)
