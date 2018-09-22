25ca5b6289d05f0093f00498aab661c0ce5d024b
FamesGER <jonasdoedtmann@hotmail.de>
2018-09-22 12:31:50 +0200

    Update botgspread.py

--- botgspread.py
+++ botgspread.py
@@ -1,5 +1,6 @@
 class botgspread():
-	def __init__(self,sheetName= 'F-Botsheet'):
+	def __init__(self,sheetName= 'F-Botsheet',sheetNumber= 0):
+		self.sNumber = sheetNumber
 		self.sName = sheetName	
 
 	def getClient(self): #create the client
@@ -15,13 +16,19 @@ class botgspread():
 		import gspread
 		from oauth2client.service_account import ServiceAccountCredentials#
 		import pprint
-		sheet = self.getClient().open(self.sName).sheet1
+		#get the sheet from the sheet name and number
+		sheetTemp = self.getClient().open(self.sName)#open main sheet
+		sheet= sheetTemp.get_worksheet(self.sNumber)#open the worksheet number with given index. 0->1->2...
 		return sheet
 
 	def delete_allrows(self):
 		sheet = self.getSheet()
 		for y in range(1,51):
 			sheet.delete_row(1)		
+
+	def delete_row(self,row=1):
+		sheet = self.getSheet()
+		sheet.delete_row(row)	
 
 	def row_val(self, row = 1):
 		sheet = self.getSheet()
@@ -42,6 +49,8 @@ class botgspread():
 		sheet= self.getSheet()
 		sheet.update_cell(row,col,val)
 
-	def row_ins(self,row=1,val=[]):
+	def row_ins(self,val=[],row=1):
 		sheet=self.getSheet()
 		sheet.insert_row(val,row)
+
+
