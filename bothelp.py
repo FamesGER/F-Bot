c61f8f5add1f562b3830a1a7007d066d3f106544
FamesGER <jonasdoedtmann@hotmail.de>
2019-05-01 13:07:52 +0200

    Update bothelp.py

--- bothelp.py
+++ bothelp.py
@@ -1,14 +1,14 @@
 def commandHelp():
 	text = ("```"
 			"!killfbot - Bot owner only.\n"
-           	"!ping - Pong! \n"
+           		"!ping - Pong! \n"
 			"!info - Info about yourself, a pinged user or the server.\n"
 			"!say - Makes the bot say something. \n"
 			"!loss - Gain.\n"
 			"!sadcat - Real sadcat hours.\n"
+			"!screamcat - Real screamcat hours.\n"
 			"!duck - latest duck tweet from @Ducks_Daily\n"
 			"!randomduck - random duck from the last 100 tweets from @LocalMallard\n"
-			"!game - list - players - add"
            "```")
 	return text
 	
